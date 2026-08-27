0e6c2df814e27a3c16033bf66a7f8ed46cba80b1
marcosamdasilva1-sudo <marcos.amdasilva1@gmail.com>
2026-08-26 23:28:24 -0300

    canvas.py
    inclusao de dados

--- 24_07/desenho_poligonos.py
+++ 24_07/desenho_poligonos.py
@@ -5,7 +5,7 @@ canvas = Canvas(janela, width=400, height=300, bg= "yellow")
 
 
 
-canvas.create_polygon(10, 50, 10, 100, 10, 150,  fill = "green")
+canvas.create_polygon(10, 10, 10, 100, 10, 150,  fill = "green")
 
 
 
