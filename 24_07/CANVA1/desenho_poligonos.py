a5e50c27164b51cf684fb88492244f8dd7765dae
marcosamdasilva1-sudo <marcos.amdasilva1@gmail.com>
2026-08-28 01:11:31 -0300

    DESENHO_POLYGONOS.PY
    INSERÇAO DE CODIGOS

--- 24_07/CANVA1/desenho_poligonos.py
+++ 24_07/CANVA1/desenho_poligonos.py
@@ -5,7 +5,14 @@ canvas = Canvas(janela, width=400, height=300, bg= "yellow")
 
 
 
-canvas.create_polygon(10, 100, 60, 50, 110, 100, 150, 80, 35, 150, fill = "green")
+canvas.create_polygon
+(10, 100,
+ 60, 50, 
+ 110, 100,
+ 150, 80,
+ 40, 150,
+ fill = "green"
+ )
 
 
 
